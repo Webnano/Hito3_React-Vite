8b1b3d914b00332040e41b6394e9551f5e994d06
NanoMedina <medina.david.bravo@gmail.com>
2024-08-24 00:30:49 -0400

    tercer Hito

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Footer from './components/Footer';
+import Home from './components/Home';
+import CardPizza from './components/CardPizza';
+
+
+function App(){
+  return (
+    <div>
+           
+      <Home />
+      <CardPizza />
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+
+
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+    <div>
+      <Navbar />
+      <Header />
+      
+       
+    </div>
+  );
+};
+
+export default Home;
